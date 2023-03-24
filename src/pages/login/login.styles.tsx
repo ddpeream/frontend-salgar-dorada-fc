e487acf53549cb743d651b1ba1103aab3dfd08fe
deimar danubio perea moreno <ddpeream@unal.edu.co>
2023-03-24 18:36:49 -0500

    login con css

--- src/pages/login/login.styles.tsx
+++ src/pages/login/login.styles.tsx
@@ -1,19 +1,95 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-image: url("../../assets/escudo-sd.jpg");
-  background-repeat: repeat;
-  background-size: 5vw 4vh;
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  place-items: center;
 `;
 
 export const ContainerLogin = styled.div`
-    
+  width: 50%;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Image = styled.img`
+  width: 100px;
+  margin-bottom: 48px;
+  justify-self: center;
+`;
+
+export const ContainerForm = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  width: 300px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  & a {
+    color: #acd9b2;
+    font-size: 14px;
+    text-align: center;
+    text-decoration: none;
+    margin-bottom: 52px;
+  }
+`;
+
+export const FormStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & > * {
+    margin: 10px 0;
+    padding: 10px 0;
+  }
+`;
+
+export const Label = styled.label`
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 4px;
+`;
+
+export const Input = styled.input`
+  background-color: #f7f7f7;
+  border: none;
+  border-radius: 8px;
+  height: 30px;
+  font-size: 16px;
+  padding: 6px;
+  margin-bottom: 12px;
+`;
+
+export const SendBoton = styled.input`
+  background-color: #acd9b2;
+  border-radius: 8px;
+  border: none;
+  color: #ffffff;
+  width: 100%;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  height: 50px;
+  margin-top: 7px;
+  margin-bottom: 15px;
+`;
+
+export const SingUpButton = styled.button`
+  margin-top: -35px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  border: 1px solid #acd9b2;
+  color: #acd9b2;
+  width: 100%;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  height: 50px;
 `;
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import { loginUser } from "../../service/login/login.service";
-import { useNavigate }  from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { LoginCredentials } from "./login-credential.interface";
-import { Container, ContainerLogin } from "./login.styles";
+import {
+  Container,
+  ContainerForm,
+  Image,
+  Form,
+  Label,
+  Input,
+  SendBoton,
+  SingUpButton,
+} from "./login.styles";
+import escudo from "../../assets/escudo-sd.jpg";
 
 export const Login = () => {
   const navigate = useNavigate();
@@ -21,31 +31,45 @@ export const Login = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('logueo: form', credentials)
+    console.log("logueo: form", credentials);
     const result = await loginUser(credentials);
     if (result) {
-      navigate('/home');
+      navigate("/home");
     } else {
       setError("Nombre de usuario o contraseña incorrectos");
     }
   };
-// const ruta_de_la_imagen = '../../assets/escudo-sd.jpg'
+  const ruta_de_la_imagen = "../../assets/escudo-sd.jpg";
+  console.log("IMAGEN: ", ruta_de_la_imagen);
   return (
     <Container>
-      hola
-      {/* <h1>Iniciar sesión</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nombre de usuario:</label>
-          <input type="text" name="username" onChange={handleInputChange} />
-        </div>
-        <div>
-          <label>Contraseña:</label>
-          <input type="password" name="password" onChange={handleInputChange} />
-        </div>
-        {error && <div>{error}</div>}
-        <button type="submit">Iniciar sesión</button>
-      </form> */}
+      <ContainerForm>
+        <Image src={escudo} />
+
+        <Form onSubmit={handleSubmit}>
+          <Label>Email addres</Label>
+          <Input
+            onChange={handleInputChange}
+            type="text"
+            name="username"
+            placeholder="example@gmail.com"
+          />
+
+          <Label>Password</Label>
+          <Input
+            onChange={handleInputChange}
+            type="password"
+            name="password"
+            placeholder="********"
+          />
+
+          <SendBoton type="submit" value="Log in" />
+
+          <a href="/">Forgot my password</a>
+        </Form>
+
+        <SingUpButton>Sign up</SingUpButton>
+      </ContainerForm>
     </Container>
   );
 };
